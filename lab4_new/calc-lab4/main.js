900c2ab4a6ab575789eafbd0206613a8d3e4430f
virokkkkkkk <72821873+virokkkkkkk@users.noreply.github.com>
2021-02-22 22:33:02 +0300

    NEW LAB4 NO EVAL

---
+++ lab4_new/calc-lab4/main.js
@@ -0,0 +1,113 @@
+function priority(operation) {
+    if (operation == '+' || operation == '-') {
+        return 1;
+    } else {
+        return 2;
+    }
+}
+
+function isNumeric(str) {
+    return /^\d+(.\d+){0,1}$/.test(str);
+}
+
+function isDigit(str) {
+    return /^\d{1}$/.test(str);
+}
+
+function isOperation(str) {
+    return /^[\+\-\*\/]{1}$/.test(str);
+}
+
+function tokenize(str) {
+    let tokens = [];
+    let lastNumber = '';
+    for (char of str) {
+        if (isDigit(char) || char == '.') {
+            lastNumber += char;
+        } else {
+            if (lastNumber.length > 0) {
+                tokens.push(lastNumber);
+                lastNumber = '';
+            }
+        }
+        if (isOperation(char) || char == '(' || char == ')') {
+            tokens.push(char);
+        }
+    }
+    if (lastNumber.length > 0) {
+        tokens.push(lastNumber);
+    }
+    return tokens;
+}
+
+function compile(str) {
+    let out = [];
+    let stack = [];
+    for (token of tokenize(str)) {
+        if (isNumeric(token)) {
+            out.push(token);
+        } else if (isOperation(token)) {
+            while (stack.length > 0 && isOperation(stack[stack.length - 1]) && priority(stack[stack.length - 1]) >= priority(token)) {
+                out.push(stack.pop());
+            }
+            stack.push(token);
+        } else if (token == '(') {
+            stack.push(token);
+        } else if (token == ')') {
+            while (stack.length > 0 && stack[stack.length - 1] != '(') {
+                out.push(stack.pop());
+            }
+            stack.pop();
+        }
+    }
+    while (stack.length > 0) {
+        out.push(stack.pop());
+    }
+    return out.join(' ');
+}
+
+function evaluate(str) {
+    let str2 = compile(str);
+
+    let stack = [];
+
+    const operators = {
+        '+': (x, y) => x + y,
+        '-': (x, y) => x - y,
+        '*': (x, y) => x * y,
+        '/': (x, y) => x / y
+    };
+
+    str2.split(' ').forEach((token) => {
+        if (token in operators) {
+            let [y, x] = [stack.pop(), stack.pop()];
+            stack.push(operators[token](x, y));
+        } else {
+            stack.push(parseFloat(token));
+        }
+    });
+
+    return stack.pop();
+}
+
+function clickHandler(event) {
+    let target = event.target;
+    if (target.tagName != 'BUTTON') return;
+    let screen = document.getElementById('screen');
+
+    if ((target.textContent == '=') || (target.textContent == 'C')) {
+        if (target.textContent == '=') {
+            screen.innerHTML = evaluate(screen.textContent).toFixed(2);
+        }
+        if (target.textContent == 'C') {
+            screen.innerHTML = null;
+        }
+    } else {
+        screen.innerHTML = screen.textContent + target.textContent;
+    }
+}
+
+window.onload = function () {
+    let calc = document.getElementById('calc');
+    calc.onclick = clickHandler;
+}
